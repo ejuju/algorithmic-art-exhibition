fe0ed14794f7c32033f0fe93225402070265d543
ejuju <julien_sellier@outlook.com>
2021-11-22 03:08:42 +0100

    added instagram link

---
+++ src/components/Footer.svelte
@@ -0,0 +1,12 @@
+<style lang="scss">
+    footer {
+        padding: 1rem;
+    }
+</style>
+
+<footer>
+    <a
+        href="https://www.instagram.com/algo.croissant/"
+        target="_blank"
+        rel="noopener noreferrer">IG</a>
+</footer>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,10 +2,10 @@
     import "../styles/manifest.scss";
 
     // import Header from "../components/Header.svelte";
-    // import Footer from "../components/Footer.svelte";
+    import Footer from "../components/Footer.svelte";
 </script>
 
 <!-- Page structure -->
 <!-- <Header /> -->
 <slot />
-<!-- <Footer /> -->
+<Footer />
